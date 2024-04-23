f6577f3c390f87a8698014f843be0e6728cbcfae
amrmohamed <amoree.mb@gmail.com>
2024-04-23 21:03:30 +0200

    Add new route to add messages and submit them

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,15 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "Mini Messageboard" });
+});
+
+router.post("/new", function (req, res, next) {
+  const message = req.body.messageText;
+  const user = req.body.user;
+  messages.push({ text: message, user: user, added: new Date() });
+  res.redirect("/");
+});
+
 module.exports = router;
